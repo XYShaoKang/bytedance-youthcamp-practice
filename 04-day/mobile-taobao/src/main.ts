3d1df4350407bdd36ae7e8296c4c989eabd8e811
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-09-24 07:43:17 +0800

    feat(04-day): mobile-taobao my brand

--- 04-day/mobile-taobao/src/main.ts
+++ 04-day/mobile-taobao/src/main.ts
@@ -1,6 +1,31 @@
 import './style.css'
 import { initSlide } from './slide'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
 initSlide()
+function initMyBrand() {
+  const data = [
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+    { imgSrc: './assets/diannao.png', title: 'Apple' },
+  ]
+  const mybrandBody = document.querySelector('.mybrand-body')
+
+  data.forEach(({ imgSrc, title }) => {
+    const item = document.createElement('li')
+    item.innerHTML = `
+      <div>
+        <img src="${imgSrc}" alt="${title}" />
+      </div>
+      <span>${title}</span>
+    `
+    mybrandBody?.append(item)
+  })
+}
+initMyBrand()
